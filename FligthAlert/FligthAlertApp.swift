2766c0949c554ea53d58673b800478c267fd1648
Akash Kottil <kottilakash@gmail.com>
2025-06-26 20:04:44 +0530

    Initial Commit

---
+++ FligthAlert/FligthAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FligthAlertApp.swift
+//  FligthAlert
+//
+//  Created by Akash Kottil on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FligthAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FligthAlertTests/FligthAlertTests.swift
@@ -0,0 +1,17 @@
+//
+//  FligthAlertTests.swift
+//  FligthAlertTests
+//
+//  Created by Akash Kottil on 26/06/25.
+//
+
+import Testing
+@testable import FligthAlert
+
+struct FligthAlertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
